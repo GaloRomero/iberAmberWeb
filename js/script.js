34a8667ae2e5313b246cd8e1f1b741edb9594a7f
GaloRomero <galoromero94@gmail.com>
2023-09-13 19:42:57 +0200

    team1 vamos

--- js/script.js
+++ js/script.js
@@ -19,7 +19,17 @@ $(document).ready(function() {
     })
 });
 
+    // Animate the scroll to top
 
+    $('.expand-button').on('click', function(){
+        $('.special-text').toggleClass('-expanded');
+        
+        if ($('.special-text').hasClass('-expanded')) {
+          $('.expand-button').html('Collapse Content');
+        } else {
+          $('.expand-button').html('Continue Reading');
+        }
+      });
 
 
 
